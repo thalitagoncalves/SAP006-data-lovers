ce0f8e98e0474271b2c4551a475fe4bdba2890ec
thagsousa15@gmail.com <thagsousa15@gmail.com>
2021-06-23 22:23:31 -0300

    Init project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/ghibli/ghibli.js';
 
-console.log(example, data);
+function showCards(data) {
+    document.getElementById('container').innerHTML = data.map((films) => `
+    <div class="cards">
+        <h3>${films.title}</h3>
+        <img src="${films.poster}">
+        <h4 class="font-lighter">Lançamento: ${films.release_date}</h4>
+        <h4 class="font-lighter">Avaliação: ${films.rt_score}</h4>
+    </div>
+    `)
+}
+
+showCards(data.films)
