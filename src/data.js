b7e037873657dc0acaae29d9455b5aaf558bc5ab
Thalita Gonçalves de Sousa <thagsousa15@gmail.com>
2022-01-13 12:37:46 -0300

    feat: add filter

--- src/data.js
+++ src/data.js
@@ -2,3 +2,10 @@ export const sortFilms = (data) => data.sort((a, b) => a.rt_score - b.rt_score);
 
 export const sortReverse = (data) => sortFilms(data).slice(0, 20).reverse()
 
+export const filterByDirector = (data, conditionValue, type) => data.filter((film) => {
+  return film[type] == conditionValue
+})
+
+export const filterFilm = (data, search) => data.filter(film => {
+  return film.name.toLowerCase().includes(search.toLowerCase());
+})
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import data from './data/ghibli/ghibli.js';
-import { sortFilms, sortReverse } from './data.js'
+import { sortFilms, sortReverse, filterByDirector, filterFilm } from './data.js'
 
 function showCards(data) {
     document.getElementById('container').innerHTML = data.map((films) => `
@@ -20,8 +20,12 @@ function showCards(data) {
 const films = data.films
 const higherScoreButton = document.getElementById('higher-score-btn');
 const lowerScoreButton = document.getElementById('lower-score-btn');
+const chooseDirector = document.querySelector('.select-director');
+const findFilm = document.getElementById('search-film');
 
 showCards(films)
 
 higherScoreButton.addEventListener('click', () => showCards(sortReverse(films)))
 lowerScoreButton.addEventListener('click', () => showCards(sortFilms(films)))
+chooseDirector.addEventListener('change', () => showCards(filterByDirector(films, chooseDirector.value, 'director')))
+findFilm.addEventListener('keypress', showCards(filterFilm(films, findFilm.value)))
